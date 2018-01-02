7853bf3c1f51b97392c091342cfdadd6988b73c6
Ria Stroes <riastroes@gmail.com>
2018-01-02 15:05:51 +0100

    Vlinders update

---
+++ Projects - 3DPrinting/Vlinders/sketch.js
@@ -0,0 +1,50 @@
+/* Ria Stroes */
+/* @updated: januari 2018  */
+
+
+var print3D;
+var maxlayers;
+var layer;
+var startlayerheight;
+var maxskirt;
+var printpath;
+var issaved;
+
+var print;
+
+function setup() {
+
+    var canvas = createCanvas(1100, 1100);
+
+    maxlayers = 1;
+    layer = 0;
+    startlayerheight = 0;
+    maxskirt = 3;
+    print3D = new Print3D("Vlinders", "Anet", "PLA", "fine", maxlayers, startlayerheight, maxskirt);
+    print3D.start();
+    print = new Print();
+
+    issaved = false;
+}
+
+function mousePressed() {
+    if (!issaved) {
+        print3D.save();
+        issaved = true;
+    }
+}
+
+function draw() {
+    if (layer < maxlayers) {
+        print.create(layer);
+        printpath = print.getPrint();
+        print3D.addToLayer(layer, printpath);
+        print3D.print(layer);
+    }
+    if (layer == maxlayers) {
+        print3D.checkPrint(printpath, 0, 0, width, height);
+        print3D.stop();
+        noLoop();
+    }
+    layer++;
+}
