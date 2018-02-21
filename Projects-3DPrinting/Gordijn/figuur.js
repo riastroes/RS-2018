92c6152dde825146a95f8613c4d9a7e6cf667364
Ria Stroes <riastroes@gmail.com>
2018-02-21 09:42:13 +0100

    update Gordijn

---
+++ Projects-3DPrinting/Gordijn/figuur.js
@@ -0,0 +1,190 @@
+//mens.js
+
+function Figuur() {
+    this.path = [];
+    this.neck = createVector(random(250, width - 250), 200);
+    this.rhand = createVector(random(width / 2, width - 50), random(50, height / 2));
+    this.lhand = createVector(random(50, width / 2), random(50, height / 2));
+    this.roksel = createVector(this.neck.x + 50, this.neck.y + 50);
+    this.loksel = createVector(this.neck.x - 50, this.neck.y + 50);
+    this.rtaile = createVector(this.neck.x + random(30, 80), height / 2);
+    this.ltaile = createVector(this.neck.x - random(30, 80), height / 2);
+
+    this.rvoet = createVector(random(width / 2, width - 50), height - 50);
+    this.lvoet = createVector(random(50, width / 2), height - 50);
+    this.rknie = createVector(random(width / 2, width - 50), random(height / 2, height / 3 * 2));
+    this.lknie = createVector(random(50, width / 2), random(height / 2, height / 3 * 2));
+    this.kruis = createVector(this.neck.x, 600);
+    this.control1 = createVector(500, -500);
+    this.control2 = createVector(width, height);
+    this.control3 = createVector(-width, height);
+
+
+
+
+}
+Figuur.prototype.create = function() {
+    var s = 3
+    for (var s = 0; s < 20; s += 3) {
+        append(this.path, this.neck);
+        append(this.path, createVector(this.neck.x - random(50, 70), this.neck.y - random(50, 100)));
+        append(this.path, createVector(this.neck.x + random(50, 70), this.neck.y - random(50, 100)));
+    }
+
+}
+Figuur.prototype.structure = function() {
+    stroke(255, 0, 0);
+    strokeWeight(5);
+    noFill();
+    point(this.neck.x, this.neck.y);
+    stroke(0);
+    strokeWeight(2);
+    noFill();
+
+    ellipse(this.neck.x, this.neck.y - 35, 75, 90);
+
+    //beginShape();
+    curve(this.control1.x, this.control1.y, this.neck.x, this.neck.y, this.rhand.x, this.rhand.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.rhand.x, this.rhand.y, this.roksel.x, this.roksel.y, this.control1.x, this.control1.y);
+    curve(this.control2.x, this.control2.y, this.roksel.x, this.roksel.y, this.rtaile.x, this.rtaile.y, this.control2.x, this.control2.y);
+    curve(this.control1.x, this.control1.y, this.rtaile.x, this.rtaile.y, this.rknie.x, this.rknie.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.rknie.x, this.rknie.y, this.rvoet.x, this.rvoet.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.rvoet.x, this.rvoet.y, this.rknie.x - 30, this.rknie.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.rknie.x - 30, this.rknie.y, this.kruis.x, this.kruis.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.kruis.x, this.kruis.y, this.lvoet.x, this.lvoet.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.lvoet.x, this.lvoet.y, this.ltaile.x, this.ltaile.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.lvoet.x, this.lvoet.y, this.ltaile.x, this.ltaile.y, this.control1.x, this.control1.y);
+    curve(this.control3.x, this.control1.y, this.ltaile.x, this.ltaile.y, this.loksel.x, this.loksel.y, this.control1.x, this.control3.y);
+    curve(this.control1.x, this.control1.y, this.loksel.x, this.loksel.y, this.lhand.x, this.lhand.y, this.control1.x, this.control1.y);
+    curve(this.control1.x, this.control1.y, this.lhand.x, this.lhand.y, this.neck.x, this.neck.y, this.control1.x, this.control1.y);
+
+    //endShape();
+
+}
+
+Figuur.prototype.draw = function() {
+    stroke(0);
+    strokeWeight(1);
+    noFill();
+    var s = 3
+    for (var s = 0; s < 20; s += 3) {
+
+
+        triangle(this.neck.x, this.neck.y, this.neck.x - random(50, 70), this.neck.y - random(50, 100), this.neck.x + random(50, 70), this.neck.y - random(50, 100))
+        triangle(this.neck.x, this.neck.y - s, this.neck.x - random(50, 70), this.neck.y - random(50, 100), this.neck.x + random(50, 70), this.neck.y - random(50, 100))
+
+        curve(this.control1.x, this.control1.y, this.neck.x, this.neck.y + s, this.rhand.x - s, this.rhand.y + s, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.rhand.x - s, this.rhand.y + s, this.roksel.x - s, this.roksel.y + s, this.control1.x, this.control1.y);
+        curve(this.control2.x, this.control2.y, this.roksel.x - s, this.roksel.y + s, this.rtaile.x - s, this.rtaile.y, this.control2.x, this.control2.y);
+        curve(this.control1.x, this.control1.y, this.rtaile.x - s, this.rtaile.y, this.rvoet.x - s, this.rvoet.y - s, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.rvoet.x - s, this.rvoet.y - s, this.kruis.x, this.kruis.y - s, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.kruis.x, this.kruis.y - s, this.lvoet.x + s, this.lvoet.y - s, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.lvoet.x + s, this.lvoet.y - s, this.ltaile.x + s, this.ltaile.y, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.lvoet.x + s, this.lvoet.y - s, this.ltaile.x + s, this.ltaile.y, this.control1.x, this.control1.y);
+        curve(this.control3.x, this.control1.y, this.ltaile.x + s, this.ltaile.y, this.loksel.x - s, this.loksel.y + s, this.control1.x, this.control3.y);
+        curve(this.control1.x, this.control1.y, this.loksel.x + s, this.loksel.y + s, this.lhand.x + s, this.lhand.y + s, this.control1.x, this.control1.y);
+        curve(this.control1.x, this.control1.y, this.lhand.x + s, this.lhand.y + s, this.neck.x, this.neck.y + s, this.control1.x, this.control1.y);
+    }
+
+
+
+}
+Figuur.prototype.print = function() {
+    stroke(0);
+    strokeWeight(1);
+    noFill();
+    var s = 3;
+    var cx, cy;
+
+    for (var s = 0; s < 20; s += 3) {
+
+        append(this.path, this.neck);
+        append(this.path, createVector(this.neck.x - random(50, 70), this.neck.y - random(50, 100)));
+        append(this.path, createVector(this.neck.x + random(50, 70), this.neck.y - random(50, 100)));
+        append(this.path, this.neck);
+
+
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.neck.x + s, this.rhand.x - s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.neck.y, this.rhand.y + s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.rhand.x - s, this.roksel.x - s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.rhand.y + s, this.roksel.y + s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control2.x, this.roksel.x - s, this.rtaile.x - s, this.control2.x, t);
+            cy = curvePoint(this.control2.y, this.roksel.y + s, this.rtaile.y, this.control2.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.rtaile.x - s, this.rvoet.x - s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.rtaile.y, this.rvoet.y - s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.rvoet.x - s, this.kruis.x + s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.rvoet.y - s, this.kruis.y - s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.kruis.x + s, this.lvoet.x + s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.kruis.y - s, this.lvoet.y - s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.lvoet.x + s, this.ltaile.x + s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.lvoet.y - s, this.ltaile.y, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control3.x, this.ltaile.x + s, this.loksel.x + s, this.control3.x, t);
+            cy = curvePoint(this.control3.y, this.ltaile.y, this.loksel.y + s, this.control3.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.loksel.x + s, this.lhand.x + s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.loksel.y + s, this.lhand.y + s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+        for (var t = 0.1; t < 1; t += 0.1) {
+            cx = curvePoint(this.control1.x, this.lhand.x + s, this.neck.x + s, this.control1.x, t);
+            cy = curvePoint(this.control1.y, this.lhand.y + s, this.neck.y + s, this.control1.y, t);
+            append(this.path, createVector(cx, cy));
+        }
+
+
+
+
+
+
+
+
+        // triangle(this.neck.x, this.neck.y, this.neck.x - random(50, 70), this.neck.y - random(50, 100), this.neck.x + random(50, 70), this.neck.y - random(50, 100))
+        // triangle(this.neck.x, this.neck.y - s, this.neck.x - random(50, 70), this.neck.y - random(50, 100), this.neck.x + random(50, 70), this.neck.y - random(50, 100))
+
+        // curve(this.control1.x, this.control1.y, this.neck.x, this.neck.y + s, this.rhand.x - s, this.rhand.y + s, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.rhand.x - s, this.rhand.y + s, this.roksel.x - s, this.roksel.y + s, this.control1.x, this.control1.y);
+        // curve(this.control2.x, this.control2.y, this.roksel.x - s, this.roksel.y + s, this.rtaile.x - s, this.rtaile.y, this.control2.x, this.control2.y);
+        // curve(this.control1.x, this.control1.y, this.rtaile.x - s, this.rtaile.y, this.rvoet.x - s, this.rvoet.y - s, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.rvoet.x - s, this.rvoet.y - s, this.kruis.x, this.kruis.y - s, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.kruis.x, this.kruis.y - s, this.lvoet.x + s, this.lvoet.y - s, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.lvoet.x + s, this.lvoet.y - s, this.ltaile.x + s, this.ltaile.y, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.lvoet.x + s, this.lvoet.y - s, this.ltaile.x + s, this.ltaile.y, this.control1.x, this.control1.y);
+        // curve(this.control3.x, this.control3.y, this.ltaile.x + s, this.ltaile.y, this.loksel.x - s, this.loksel.y + s, this.control1.x, this.control3.y);
+        // curve(this.control1.x, this.control1.y, this.loksel.x + s, this.loksel.y + s, this.lhand.x + s, this.lhand.y + s, this.control1.x, this.control1.y);
+        // curve(this.control1.x, this.control1.y, this.lhand.x + s, this.lhand.y + s, this.neck.x, this.neck.y + s, this.control1.x, this.control1.y);
+    }
+
+
+
+}
+Figuur.prototype.drawPrint = function() {
+
+    for (var i = 1; i < this.path.length; i++) {
+        line(this.path[i - 1].x, this.path[i - 1].y, this.path[i].x, this.path[i].y);
+    }
+
+}
---
+++ Projects-3DPrinting/Gordijn/sketch.js
@@ -0,0 +1,280 @@
+/* Ria Stroes */
+/* @updated: november 2017  */
+/* Metro
+ */
+
+
+var grid;
+var margew, margeh;
+
+var print3D;
+var layer;
+var maxlayers;
+var show;
+var printpath;
+var offset;
+
+var iscreated;
+var issaved;
+var isready;
+var model;
+
+
+var name;
+var pos;
+var path;
+var next;
+
+var figuur;
+
+
+function setup() {
+
+    var canvas = createCanvas(1000, 1000);
+    isready = false;
+
+    windowscale = 1;
+    printpath = [];
+    path = [];
+
+
+    layer = 0;
+    maxlayers = 1;
+    var startlayerheight = 1; // 1
+    print3D = new Print3D("Gordijn", "Anet", "SAT1N", "normal", maxlayers, startlayerheight);
+
+    frameRate(1);
+    isready = false;
+    issaved = false;
+
+}
+
+
+function draw() {
+
+    if (!iscreated) {
+        background(255);
+        push();
+        scale(0.7);
+        for (var i = 0; i < 2; i++) {
+
+            figuur = new Figuur();
+            figuur.create();
+            figuur.print();
+            //figuur.drawPrint();
+            printpath = printpath.concat(figuur.path);
+        }
+        pop();
+        iscreated = true;
+    } else {
+        if (layer < maxlayers) {
+
+            print3D.start();
+            print3D.addPointToLayer(layer, createVector(150, 30), true);
+            print3D.addPointToLayer(layer, createVector(750, 50), true);
+            print3D.addPointToLayer(layer, createVector(550, 50), true);
+            print3D.addToLayer(layer, printpath);
+
+            //print3D.addPointToLayer(layer, createVector(990, 990), true);
+            print3D.print(layer);
+            isready = true;
+
+        }
+        if (layer + 1 == maxlayers) {
+            print3D.stop();
+            noLoop();
+            isready = true;
+        }
+        layer++;
+    }
+
+
+
+
+
+}
+
+function mousePressed() {
+    if (!issaved && isready) {
+        print3D.save();
+        issaved = true;
+    }
+}
+
+function reversePath(path) {
+    var reverse = [];
+    for (var i = path.length - 1; i >= 0; i--) {
+        append(reverse, path[i]);
+    }
+    return reverse;
+}
+
+function findPath(p, maxdepth) {
+    var i = (p.y * 1000 * 4) + (p.x * 4);
+    model.pixels[i] = 255;
+    model.pixels[i + 1] = 0;
+    model.pixels[i + 2] = 0;
+    model.pixels[i + 3] = 255;
+    var apath = [];
+
+    append(apath, p);
+    var subpath = [];
+    var longest = 0;
+    for (let b = 0; b < 8; b++) {
+        var np = p.copy().add(dx[b], dy[b]);
+        var npi = (np.y * 1000 * 4) + (np.x * 4);
+        if (maxdepth > 0 && model.pixels[npi] < 50 && model.pixels[npi + 1] < 50 && model.pixels[npi + 2] < 50) {
+            subpath[b] = findPath(np, maxdepth - 1);
+
+        }
+
+    }
+
+    var longest = 0;
+    var i;
+    for (let b = 0; b < 8; b++) {
+
+        if (subpath[b] && subpath[b].length > longest) {
+            longest = subpath[b].length;
+            i = b;
+        }
+    }
+    if (longest > 0) {
+        apath = apath.concat(subpath[i]);
+        last = apath[apath.length - 1];
+    }
+
+    return apath;
+}
+
+
+function findStart(start) {
+
+
+    var found = false;
+    var pos = createVector(0, 0);
+    var maxdis = 20;
+    var min = 0;
+
+    //first black pixel
+    for (var i = 0; i < model.pixels.length; i += 4) {
+        pos.x = floor((i / 4) % 1000);
+        pos.y = floor((i / 4) / 1000);
+        if (checkColor(pos)) {
+            if (dist(start.x, start.y, pos.x, pos.y) < maxdis) {
+                maxdis = dist(start.x, start.y, pos.x, pos.y);
+                min = i;
+                found = true;
+            }
+        }
+    }
+    if (found) {
+        model.pixels[min] = 255;
+        model.pixels[min + 1] = 0;
+        model.pixels[min + 2] = 0;
+        model.pixels[min + 3] = 255;
+        pos.x = floor((min / 4) % 1000);
+        pos.y = floor((min / 4) / 1000);
+
+    }
+    return pos;
+}
+
+function getMaxPath() {
+    var foundmax = 0;
+    var max = 0;
+
+    var m = 0;
+
+
+    for (var i = 0; i < path.length; i++) {
+        if (max <= path[i].length) {
+            max = path[i].length;
+            foundmax = i;
+            amax[m] = max;
+            m++;
+        }
+    }
+
+}
+
+function drawPath(path) {
+    let i;
+    let n;
+    for (n = 0; n < path.length; n++) {
+        i = (path[n].y * 1000 * 4) + (path[n].x * 4);
+
+        model.pixels[i] = 255;
+        model.pixels[i + 1] = 0;
+        model.pixels[i + 2] = 255;
+    }
+
+}
+
+function showPath(path, acolor) {
+    let i;
+    for (i = 0; i < path.length; i++) {
+        stroke(acolor);
+        ellipse(path[i].x + offset.x, path[i].y + offset.y, 2, 2);
+    }
+    ellipse(path[i - 1].x + offset.x, path[i - 1].y + offset.y, 6, 6);
+
+}
+
+function showPoint(vector, acolor) {
+    stroke(acolor);
+    fill(acolor);
+    ellipse(vector.x + offset.x, vector.y + offset.y, 6, 6);
+
+}
+
+
+function contains(a, obj) {
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkColor(pos) {
+    let found = false;
+    let i = (pos.y * 1000 * 4) + (pos.x * 4);
+    let c = new Rgb(model.pixels[i], model.pixels[i + 1], model.pixels[i + 2]);
+
+    if (compareRGBColors(c, ablack, 50)) {
+        found = true;
+    }
+
+    return found;
+}
+
+function compareRGBColors(acolor, bcolor, colormarge) {
+    let ok = false;
+    if (abs(acolor.r - bcolor.r) < 50 && abs(acolor.g - bcolor.g) < 50 && abs(acolor.b - bcolor.b) < 50) {
+        ok = true;
+    }
+    return ok;
+}
+
+function compareColors(acolor, bcolor, colormarge) {
+    let ok = false;
+    if (abs(red(acolor) - red(bcolor) < colormarge) &&
+        abs(green(acolor) - green(bcolor) < colormarge) &&
+        abs(blue(acolor) - blue(bcolor) < colormarge)) {
+        ok = true;
+    }
+    return ok;
+}
+
+function inPos(next) {
+    var found = false;
+    for (var i = 0; i < pos.length; i++) {
+        if (pos[i].x == next.x && pos[i].y == next.y) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
