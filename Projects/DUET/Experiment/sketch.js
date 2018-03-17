bc8d39667adfe5f095234742521288cf6d23a729
riastroes <riastroes@gmail.com>
2018-03-17 19:32:39 +0100

    update Duet

--- Projects/DUET/Experiment/sketch.js
+++ Projects/DUET/Experiment/sketch.js
@@ -11,17 +11,131 @@ var offset;
 var imgpixels;
 
 var img;
+
 var isfilterd;
 var resultimg;
-var stamp;
+
 var softborder;
+var canvasinspiration;
+var inspiration;
+
+//pattern
+var cols;
+var rows;
+
+//stamp
+var stamp;
+var stampwidth;
+var stampheight;
+
+var canvassample;
 
 function changeSettings() {
-    alert("hoi");
-    var canvaswidth = document.getElementById("incanvaswidth")
-    var canvasheight = document.getElementById("incanvasheight")
+    //layout
+    var layout = document.getElementById("layout");
+    layout.style.display ="block";
+    //canvas
+    var canvaswidth = document.getElementById("incanvaswidth");
+    var canvasheight = document.getElementById("incanvasheight");
     canvas = createCanvas(parseInt(canvaswidth.value), parseInt(canvasheight.value));
-    background(30, 50, 50);
+    
+    var divcanvas = document.getElementById("divcanvas");
+    divcanvas.style.width = (windowWidth - 400) + "px";
+    divcanvas.style.height = (windowHeight) + "px";
+    canvas.parent(divcanvas);
+    
+    
+    //inspirational image
+    
+    
+
+    var imginspiration = document.getElementById("inspiration");
+    imginspiration.width = 360;
+    imginspiration.height = 240;
+
+    var imgbiginspiration = document.getElementById("biginspiration");
+    imgbiginspiration.width = 3600;
+    imgbiginspiration.height = 2400;
+    
+    
+    canvasinspiration = document.getElementById("canvasinspiration");
+    canvasinspiration.width = 360;
+    canvasinspiration.height = 240;
+    var ctx = canvasinspiration.getContext("2d");
+
+    canvasbiginspiration = document.getElementById("canvasbiginspiration");
+    canvasbiginspiration.width = 3600;
+    canvasbiginspiration.height = 2400;
+    var ctxbig = canvasbiginspiration.getContext("2d");
+    
+    ctx.drawImage(imginspiration,0,0,imginspiration.width,imginspiration.height);
+    ctxbig.drawImage(imgbiginspiration,0,0,imgbiginspiration.width,imgbiginspiration.height);
+    inspiration = ctxbig.getImageData(0,0,canvasbiginspiration.width,canvasbiginspiration.height);
+    
+    canvasinspiration.onclick = function(){
+         //
+        var i = (floor(event.offsetX-10) * this.width * 4) + (floor(event.offsetX-10) * 4);
+        console.log(i);
+        
+        var param = rgbToHsl(inspiration.data[i], inspiration.data[i + 1], inspiration.data[i + 2]);
+        var rgbcolor = color(inspiration.data[i], inspiration.data[i + 1], inspiration.data[i + 2], 255);
+        var hslcolor = color(param[0] * 360, param[1] * 100, param[2] * 100);
+       
+        
+        var ahue = hue(hslcolor);
+        var range = 30;
+        if(ahue >= 0 && ahue <= 360){
+            colorstrip.add(hslcolor);
+            var x = map(event.offsetX-10, 0, canvasinspiration.width, 0, canvasbiginspiration.width);
+            var y = map(event.offsetY-20, 0, canvasinspiration.height, 0, canvasbiginspiration.height);
+            stamp.loadInk(canvasbiginspiration, inspiration, floor(x), floor(y), ahue, range);
+            stamp.mask(softborder);
+
+            var ctxsample = canvassample.getContext("2d");
+    
+            var imgData = ctxsample.getImageData(0,0,stampwidth, stampheight );
+            for(var i = 0; i < imgData.data.length; i+=4){
+                imgData.data[i] = 255;
+                imgData.data[i+1] = 255;
+                imgData.data[i+2] = 255;
+                imgData.data[i+3] = 255;
+            }
+            stamp.getData(imgData);
+            ctxsample.putImageData(imgData, 0,0);
+        }
+        
+
+         
+     }
+    //pattern
+    cols = document.getElementById("inpatterncols").value;
+    rows = document.getElementById("inpatternrows").value;
+
+    //stamp
+   
+
+    stampwidth = parseInt(document.getElementById("instampwidth").value);
+    stampheight = parseInt(document.getElementById("instampheight").value);
+    stamp = new Stamp(stampwidth,stampheight);
+
+    canvassample = document.getElementById("canvassample");
+    canvassample.width = stampwidth;
+    canvassample.height = stampheight;
+    
+    var ctxsample = canvassample.getContext("2d");
+    
+    var imgData = ctxsample.getImageData(0,0,stampwidth, stampheight );
+    for(var i = 0; i < imgData.data.length; i+=4){
+     imgData.data[i] = 255;
+     imgData.data[i+1] = 255;
+     imgData.data[i+2] = 255;
+     imgData.data[i+3] = 255;
+    }
+    ctxsample.putImageData(imgData,0,0);
+    
+    
+    
+
 }
 
 function preload() {
@@ -33,23 +147,18 @@ function preload() {
     img[4] = loadImage("images/img5.jpg");
     img[5] = loadImage("images/img6.jpg");
     softborder = loadImage("images/softborder200x200.png");
+    //softborder = loadImage("images/triangle200x200.png");
 }
 
 function setup() {
     pixelDensity(1);
-    canvas = createCanvas(windowWidth - 400, windowHeight);
-    var td = document.getElementById("tdcanvas");
-    canvas.parent(td);
-
-    img[0].resize(width, height);
-    imageMode(CENTER);
-    image(img[0], width / 2, height / 2);
-    loadPixels();
-    background(255);
+    changeSettings();
+    
     var imginspiration = document.getElementById("imgInspiration");
-    imginspiration.src = "images/img8.jpg";
-    imginspiration.width = 360;
-    imginspiration.height = auto;
+    
+   // imginspiration.src =  "images/img6.jpg";
+   // imginspiration.width = 300;
+   
 
 
     colorstrip = new ColorStrip(createVector(0, 0), width, 50);
@@ -60,7 +169,7 @@ function setup() {
 
     offset = createVector(50, 50);
     imgpixels = [];
-    stamp = new Stamp(200, 200);
+    
 
 
 
@@ -73,39 +182,35 @@ function draw() {
     //background(255);
 
 
-    if (stamp.isloaded) {
+   // if (stamp.isloaded) {
 
         //stamp.draw(random(-stamp.width/2, this.width + (stamp.width/2)), random(-stamp.height/2, this.height+(stamp.height/2)));
-    }
+   //}
 
 
     colorstrip.show();
 }
 
 function mousePressed() {
-    stamp.draw(mouseX, mouseY);
+    if(mouseX >0 && mouseY >0 && mouseX < width && mouseY<height){
+        var w = canvas.width/cols;
+        var px = (mouseX%w) - (stampwidth/2);
+        var h = canvas.height/rows;
+        var py = (mouseY%h) - (stampheight/2);
+        for(var i = -1; i <= cols; i++){
+            for(var j = -1; j <= rows; j++){
+                stamp.draw((j*w) + px ,(i*h) + py);
+            }
+        }
+        
+    }
+    
 
 }
 
 
 function keyPressed() {
-    if (keyCode === SHIFT) {
-        var i = (floor(mouseY) * this.width * 4) + (floor(mouseX) * 4);
-        console.log(i);
-        img[0].loadPixels();
-
-        var param = rgbToHsl(img[0].pixels[i], img[0].pixels[i + 1], img[0].pixels[i + 2]);
-        var rgbcolor = color(img[0].pixels[i], img[0].pixels[i + 1], img[0].pixels[i + 2], 255);
-        var hslcolor = color(param[0] * 360, param[1] * 100, param[2] * 100);
-        colorstrip.add(hslcolor);
-        console.log(hue(rgbcolor), hue(hslcolor));
-        var ahue = hue(hslcolor);
-        var range = 10;
-
-        stamp.loadInk(img[0], floor(mouseX), floor(mouseY), ahue, range);
-        stamp.mask(softborder);
-
-    } else if (keyCode === BACKSPACE) {
+    if (keyCode === BACKSPACE) {
         background(255);
     } else if (keyCode === UP_ARROW) {
         stamp.grow(10);
--- Projects/DUET/Experiment/stamp.js
+++ Projects/DUET/Experiment/stamp.js
@@ -1,8 +1,9 @@
-function Stamp(size) {
-    this.width = size;
-    this.height = size;
+function Stamp( stampwidth, stampheight) {
+    this.width = stampwidth;
+    this.height = stampheight;
     this.isloaded = false;
     this.image;
+    
     this.pixels = this.init();
 
 }
@@ -11,11 +12,12 @@ Stamp.prototype.init = function() {
 
     this.image = createImage(this.width, this.height);
     this.image.loadPixels();
+    var density = this.image.pixels.length / this.width / this.height / 4;
     for (var i = 0; i < this.image.pixels.length; i += 4) {
         this.image.pixels[i] = 0;
         this.image.pixels[i + 1] = 0;
         this.image.pixels[i + 2] = 0;
-        this.image.pixels[i + 3] = 0;
+        this.image.pixels[i + 3] = 0;//transparent
     }
     this.image.updatePixels();
 
@@ -33,17 +35,16 @@ Stamp.prototype.shrink = function(force) {
     this.init();
 }
 
-Stamp.prototype.loadInk = function(img, px, py, hue, range) {
+Stamp.prototype.loadInk = function(acanvas, inspiration, px, py, hue, range) {
     // load this.pixels with the hue at position x,y on a given image.
     // if (px < this.width) { px = this.width }
-    // if (px > width - this.width) { px = img.width - this.width }
+    // if (px > acanvas.width - this.width) { px = img.width - this.width }
     // if (py < this.height) { py = this.height }
-    // if (py > height - this.height) { py = height - this.height }
+    // if (py > acanvas.height - this.height) { py = acanvas.height - this.height }
     var x = floor(px);
     var y = floor(py);
-
-    //image(img, width/2, height/2);
-    //loadPixels();
+    console.log(x,y);
+    
     this.pixels = this.init();
 
 
@@ -52,42 +53,31 @@ Stamp.prototype.loadInk = function(img, px, py, hue, range) {
     var rgb = [];
     var i;
     var j = 0;
-    var density = ((this.image.pixels.length / this.width) / this.height) / 4;
-    console.log(density);
+    
+    var stampforce = 0;
 
     for (var a = y; a < (y + this.height); a++) {
         for (var b = x; b < (x + this.width); b++) {
-            i = (a * width * 4) + (b * 4);
-            j = ((a - y) * this.image.width * 4) + ((b - x) * 4);
+            i = (a * acanvas.width * 4) + (b * 4);
+            //j = ((a - y) * this.image.width * 4) + ((b - x) * 4);
 
-            param = rgbToHsl(pixels[i], pixels[i + 1], pixels[i + 2]);
+            param = rgbToHsl(inspiration.data[i], inspiration.data[i + 1], inspiration.data[i + 2]);
             if ((param[0] * 360) > (hue - range) && (param[0] * 360) < hue + range) {
-                this.image.pixels[j] = pixels[i];
-                this.image.pixels[j + 1] = pixels[i + 1];
-                this.image.pixels[j + 2] = pixels[i + 2];
+                this.image.pixels[j] = inspiration.data[i];
+                this.image.pixels[j + 1] = inspiration.data[i + 1];
+                this.image.pixels[j + 2] = inspiration.data[i + 2];
                 this.image.pixels[j + 3] = 255;
-                this.isloaded = true;
+                stampforce++;
+                
             }
-
-
-
-
-            // param = rgbToHsl(img.pixels[i], img.pixels[i + 1], img.pixels[i + 2]);
-
-            // if ((param[0] * 360) > (hue - range) && (param[0] * 360) < hue + range) {
-            //     //param[0] = (hue / 360);
-
-            //     rgb = hslToRgb(param[0], param[1], param[2]);
-            //     this.image.pixels[j] = rgb[0];
-            //     this.image.pixels[j + 1] = rgb[1];
-            //     this.image.pixels[j + 2] = rgb[2];
-            //     this.image.pixels[j + 3] = 255;
-            //     this.isloaded = true;
-            // }
-
+            j+=4;
         }
     }
-    this.image.updatePixels();
+    if(stampforce > 0){
+        this.image.updatePixels();
+    }
+    
+    console.log("stampforce", stampforce)
 
 }
 Stamp.prototype.draw = function(x, y) {
@@ -96,4 +86,10 @@ Stamp.prototype.draw = function(x, y) {
 Stamp.prototype.mask = function(img) {
     //img.resize(this.width, this.height);
     this.image.mask(img);
+}
+Stamp.prototype.getData = function(imgdata){
+    for(var i = 0; i < this.image.pixels.length; i++){
+            imgdata.data[i] = this.image.pixels[i];
+    }
+    
 }
