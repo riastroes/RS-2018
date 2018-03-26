49b26154be857e99d31022d07a4e21b41c57a03f
Ria Stroes <riastroes@gmail.com>
2018-03-26 21:50:51 +0200

    Duet update

--- Projects/DUET/PatternDesigner2/design.js
+++ Projects/DUET/PatternDesigner2/design.js
@@ -16,7 +16,7 @@ Design.prototype.background = function(acolor) {
 Design.prototype.stamp = function() {
     var imgData = this.ctx.createImageData(stamp.width, stamp.height);
 
-    
+
     var x = event.offsetX;
     var y = event.offsetY;
 
@@ -28,47 +28,68 @@ Design.prototype.stamp = function() {
         for (var j = -1; j <= this.rows; j++) {
             var ax = (i * w) + px;
             var ay = (j * h) + py;
-            var bg = this.ctx.getImageData(ax,ay, stamp.width, stamp.height);
-            for(var p = 0; p < bg.data.length;p+=4){
-                if(stamp.stampData.data[p+3] != 255){
+            var bg = this.ctx.getImageData(ax, ay, stamp.width, stamp.height);
+            for (var p = 0; p < bg.data.length; p += 4) {
+                if (stamp.stampData.data[p + 3] != 255) {
                     imgData.data[p] = bg.data[p];
-                    imgData.data[p+1] = bg.data[p+1];
-                    imgData.data[p+2] = bg.data[p+2];
-                    imgData.data[p+3] = bg.data[p+3];
-                }
-                else if(stamp.stampData.data[p] ==0 && stamp.stampData.data[p+1] ==0 && stamp.stampData.data[p+2] ==0 && stamp.stampData.data[p+3] ==0 ){
+                    imgData.data[p + 1] = bg.data[p + 1];
+                    imgData.data[p + 2] = bg.data[p + 2];
+                    imgData.data[p + 3] = bg.data[p + 3];
+                } else if (stamp.stampData.data[p] == 0 && stamp.stampData.data[p + 1] == 0 && stamp.stampData.data[p + 2] == 0 && stamp.stampData.data[p + 3] == 0) {
                     //empty stamp
                     imgData.data[p] = bg.data[p];
-                    imgData.data[p+1] = bg.data[p+1];
-                    imgData.data[p+2] = bg.data[p+2];
-                    imgData.data[p+3] = bg.data[p+3];
-               }
-                else {
+                    imgData.data[p + 1] = bg.data[p + 1];
+                    imgData.data[p + 2] = bg.data[p + 2];
+                    imgData.data[p + 3] = bg.data[p + 3];
+                } else {
                     imgData.data[p] = stamp.stampData.data[p];
-                    imgData.data[p+1] = stamp.stampData.data[p+1];
-                    imgData.data[p+2] = stamp.stampData.data[p+2];
-                    imgData.data[p+3] = stamp.stampData.data[p+3];
+                    imgData.data[p + 1] = stamp.stampData.data[p + 1];
+                    imgData.data[p + 2] = stamp.stampData.data[p + 2];
+                    imgData.data[p + 3] = stamp.stampData.data[p + 3];
                 }
             }
-            
+
             this.ctx.putImageData(imgData, ax, ay);
-            
+
 
         }
     }
-    
-   
+
+
 
 
 }
-Design.prototype.save = function(){
+Design.prototype.save = function() {
+    var br1 = document.createElement("BR");
     var acanvas = document.createElement("canvas");
     var parent = document.getElementById("divdesigns");
-    acanvas.parent(parent);
-    acanvas.width   =500;
-    acanvas.height = 500;
+    parent.appendChild(br1);
+    parent.appendChild(acanvas);
+    acanvas.width = this.canvas.width;
+    acanvas.height = this.canvas.height
+
+    var imgData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+
     var ctx = acanvas.getContext('2d');
-    ctx.fillStyle = "ff0000";
-    ctx.fillRect(0, 0, acanvas.width, acanvas.height);
+    ctx.putImageData(imgData, 0, 0);
+    ctx.scale(0.1, 0.1);
+
+    var link = document.createElement('a');
+    link.className = "rs-button";
+    var br2 = document.createElement("BR");
+    link.innerHTML = 'download JIP-pattern';
+    link.addEventListener('click', function(ev) {
+        if (link.className != "issaved") {
+            link.href = acanvas.toDataURL();
+            link.download = "JIP-pattern.jpg";
+            link.className = "issaved";
+        }
+
+    }, false);
+
+    parent.appendChild(br2);
+    parent.appendChild(link);
+
+
 
 }
--- Projects/DUET/PatternDesigner2/sketch.js
+++ Projects/DUET/PatternDesigner2/sketch.js
@@ -11,20 +11,22 @@ var stamp;
 var palette;
 var design;
 
+"use strict";
+
 
 function start() {
-    
+
     inspiration = new Inspiration();
     palette = new Palette();
     stamp = new Stamp();
     design = new Design();
     inspiration.changeInspiration(1);
     stamp.changeStamp(1);
-    
+
 
 }
 
-function calc(){
+function calc() {
     var width = document.getElementById('main').offsetWidth;
     alert(width);
 }
